4d4d7c6cc6fcd0d6bbde5065b4d6687ea4555146
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2015-07-06 15:17:42 +0200

    Initial commit: brackets plugin skeleton

---
+++ main.js
@@ -0,0 +1,13 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, regexp: true, bitwise: true */
+/*global define, CodeMirror, brackets, less, $, WebSocket, window */
+
+define(function (require, exports, module) {
+    "use strict";
+    
+    var AppInit = brackets.getModule('utils/AppInit');
+
+    AppInit.appReady(function () {
+
+    });
+    
+});
