ad4af335ff8dafdcea2dcda96d1e1d66e8e545b1
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2015-07-29 15:50:11 +0200

    Loading a config file

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ define(function (require, exports, module) {
 	var Dialogs = brackets.getModule("widgets/Dialogs");
   	var DefaultDialogs = brackets.getModule("widgets/DefaultDialogs");
     
-    var FileViewController = brackets.getModule('project/FileViewController');
-    var ProjectManager = brackets.getModule('project/ProjectManager');
+    var FileViewController = brackets.getModule("project/FileViewController");
+    var ProjectManager = brackets.getModule("project/ProjectManager");
 
     AppInit.appReady(function () {
     	CommandManager.register("Start Fiddlets Study", "Fiddlets.Study.start", startStudy);
@@ -22,8 +22,13 @@ define(function (require, exports, module) {
     	debugMenu.addMenuItem("Fiddlets.Study.start");
     	console.log("FiddletsStudy", "App ready...");
 
+        var projectRoot = ProjectManager.getProjectRoot();
+        var configText = require("text!tasksConfig.json");
+        var config = JSON.parse(configText);
+        console.log(JSON.stringify(config));
+
         ProjectManager.on("projectOpen", function() {
-            var projectRoot = ProjectManager.getProjectRoot();
+            
             console.log("Opened project root: " + projectRoot.fullPath);
 
             FileViewController.openFileAndAddToWorkingSet(projectRoot.fullPath + "/mustache.js");
